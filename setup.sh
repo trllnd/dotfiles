c249d4c71b01281cce631469d5fcc66562433aa8
trllnd <trllnd@cbrspc>
2019-03-07 00:23:20 +0100

    wip

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 echo "\$UP' G ================================================================="
 
 echo "apt-get install:"
-echo "- i3"
-echo "- i3blocks"
-echo "- i3lock"
-echo "- variety"
-echo "- rofi"
 echo "- compton"
-echo "- rxvt-unicode"
+echo "- i3"
+echo "- i3lock"
 echo "- neofetch"
 echo "- poweline"
+echo "- rofi"
 echo "- termite"
+echo "- variety"
 echo ""
 
 echo "to set theme:"
@@ -18,16 +16,18 @@ echo "sudo apt-get install lxappearance gtk-chtheme qt4-qtconfig"
 
 dir=$(realpath $(dirname $0))
 
+mkdir -v -p ~/.config/base16-shell
+mkdir -v -p ~/.config/compton
 mkdir -v -p ~/.config/i3
 mkdir -v -p ~/.config/rofi
-mkdir -v -p ~/.config/base16-shell
 mkdir -v -p ~/.config/termite
 
-ln -v -s -f -t ~/.config/i3           $dir/.config/i3/*
 ln -v -s -f -t ~                      $dir/.i3blocks.conf
-ln -v -s -f -t ~/.config/termite      $dir/.config/termie/*
-ln -v -s -f -t ~/.config/rofi         $dir/.config/rofi/*
 ln -v -s -f -t ~                      $dir/.vimrc
 ln -v -s -f -t ~                      $dir/.zshrc
 ln -v -s -f -t ~/.config/base16-shell $dir/.config/base16-shell/*
+ln -v -s -f -t ~/.config/compton      $dir/.config/compton/*
+ln -v -s -f -t ~/.config/i3           $dir/.config/i3/*
+ln -v -s -f -t ~/.config/rofi         $dir/.config/rofi/*
+ln -v -s -f -t ~/.config/termite      $dir/.config/termite/*
 
